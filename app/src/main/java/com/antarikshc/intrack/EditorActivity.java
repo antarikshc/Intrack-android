d71a8a8dc945c7f03aa0eca3648cf5cba4c64c98
Antariksh C <antarikshc@gmail.com>
2018-05-28 22:49:38 +0530

    Added Intents for Camera and Gallery
    Signed-off-by: Antariksh C <antarikshc@gmail.com>

--- app/src/main/java/com/antarikshc/intrack/EditorActivity.java
+++ app/src/main/java/com/antarikshc/intrack/EditorActivity.java
@@ -1,17 +1,26 @@
 package com.antarikshc.intrack;
 
+import android.content.Intent;
 import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.view.ViewCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatEditText;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class EditorActivity extends AppCompatActivity {
+
+    ImageView itemIcon;
 
     TextView editImage;
 
@@ -30,11 +39,19 @@ public class EditorActivity extends AppCompatActivity {
     AppCompatEditText editSupplierEmail;
     ImageView supEmailIcon;
 
+    // ID for Image intents
+    public static final int REQUEST_IMAGE_CAPTURE = 1;
+    public static final int PICK_IMAGE = 2;
+
+    Intent pictureIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
 
+        itemIcon = findViewById(R.id.item_icon);
+        editImage = findViewById(R.id.edit_image);
         itemName = findViewById(R.id.item_name_text);
         editItemName = findViewById(R.id.item_name_edit);
         stockAmount = findViewById(R.id.stock_amount_text);
@@ -56,6 +73,46 @@ public class EditorActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.startAnimation(buttonClick);
+
+                LayoutInflater inflater = LayoutInflater.from(getApplicationContext());
+                View dialogView = inflater.inflate(R.layout.edit_image_layout, null);
+                final AlertDialog dialog = new AlertDialog.Builder(EditorActivity.this).create();
+
+                dialog.setView(dialogView);
+
+                // we don't want title
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+                dialog.setContentView(R.layout.edit_image_layout);
+
+                // Bind views from dialog layout
+                TextView clickPhoto = dialogView.findViewById(R.id.click_photo);
+                TextView choosePhoto = dialogView.findViewById(R.id.choose_photo);
+
+                clickPhoto.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        pictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        if (pictureIntent.resolveActivity(getPackageManager()) != null) {
+                            startActivityForResult(pictureIntent, REQUEST_IMAGE_CAPTURE);
+                        }
+
+                        dialog.dismiss();
+                    }
+                });
+
+                choosePhoto.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        pictureIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                        pictureIntent.setType("image/*");
+                        startActivityForResult(pictureIntent, PICK_IMAGE);
+
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
             }
         });
 
@@ -139,6 +196,74 @@ public class EditorActivity extends AppCompatActivity {
                 }
             }
         });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Bitmap imageBitmap;
+        Bitmap croppedImage;
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+
+            try {
+                croppedImage = cropImage(imageBitmap);
+                itemIcon.setImageBitmap(croppedImage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } else if (requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
+
+            Uri uri = data.getData();
+            try {
+                imageBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                croppedImage = cropImage(imageBitmap);
+                itemIcon.setImageBitmap(croppedImage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private Bitmap cropImage(Bitmap ogBitmap) {
+
+        Bitmap croppedImage;
+
+        int width = ogBitmap.getWidth();
+        int height = ogBitmap.getHeight();
+
+        // Get the 4:3 aspect ratio to compare with Bitmap ratio
+        float aspectRatio = 4f / 3f;
+        float bitmapRatio;
+
+        if (width >= height) {
+            // this means bitmap is horizontally oriented
+            bitmapRatio = (float) width / (float) height;
+
+            if (bitmapRatio == aspectRatio) {
+                // If the Bitmap is 4:3 don't crop
+                croppedImage = ogBitmap;
+            } else {
+                croppedImage = Bitmap.createBitmap(ogBitmap, (width / 2 - height - 2), 0, height, height);
+            }
+
+        } else {
+            // this means bitmap is vertically oriented
+            bitmapRatio = (float) height / (float) width;
+            if (bitmapRatio == aspectRatio) {
+                croppedImage = ogBitmap;
+            } else {
+                croppedImage = Bitmap.createBitmap(ogBitmap, 0, (height / 2 - width / 2), width, width);
+            }
+        }
+
+        return croppedImage;
 
     }
 
